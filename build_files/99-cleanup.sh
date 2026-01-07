bcf7678ffca387d5526dd3012d4467a53733b432
Hec <hec@heccraft.com>
2026-01-06 20:56:18 -0500

    actually cleanup

--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -5,5 +5,9 @@ set -xeuo pipefail
 # Cleanup pacman directories after installation
 rm -rf /usr/lib/sysimage/log /usr/lib/sysimage/cache/pacman/pkg
 
+# bootc
+rm -rf /boot /var
+mkdir -p /boot /var
+
 # Remove /etc/resolv.conf, we use tmpfiles.d to link it to /run/systemd/resolve/resolv.conf
 rm -f /etc/resolv.conf
